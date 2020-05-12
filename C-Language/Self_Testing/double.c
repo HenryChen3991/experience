a4b48b1803467a0d2df8668e39db6418f35c9e6e
henry_chen <henry_chen@gemteks.com>
2020-05-12 11:50:05 +0800

    [C][Self_Testing] Add self_testing dir

---
+++ C-Language/Self_Testing/double.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+int a=-1;
+void aaa()
+{
+	if(a != 1 && a!= 3) 
+		printf("a = %d\n",a);
+	else{
+		printf("xx\n");
+		return ;
+	}
+	printf("+++++\n");
+}
+
+void main(void)
+{
+
+	double i = 5111222333444555;
+	printf("i = %.0lf\n",i);
+	while(1){
+		aaa();
+		a++;
+		sleep(2);
+	}
+}
---
+++ C-Language/Self_Testing/mod.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    unsigned int i = 187702916684;
+    printf("%u\n",i%4294967296);
+}
