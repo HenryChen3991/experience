d6e06d8aa5c7d74ea7cf7127d488a679f2b1efc2
henry_chen <henry_chen@gemteks.com>
2022-01-11 18:23:33 +0800

    [C][OMCI][OMCI-To-PCAP] Support OMCI-To-PCAP tool v1.0.2
    [Description]
         1.modify run.sh so that user only need to modify OBJ attr.

--- C-Language/OMCI/OMCI-To-PCAP/run.sh
+++ C-Language/OMCI/OMCI-To-PCAP/run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+OBJ="omci-pa"
+
+
 make clean
 
 make
 
-./omci2pcap omci.txt  omci.pcap.format
+./omci2pcap $OBJ.txt  $OBJ.pcap.format
 
 echo -e "\nOmci.pcap:"
 #cat omci.pcap.format
 
-echo -e "\ntext2pcap omci.pcap.format omci.pcap:"
-text2pcap omci.pcap.format omci.pcap
+echo -e "\ntext2pcap $OBJ.pcap.format $OBJ.pcap:"
+text2pcap $OBJ.pcap.format $OBJ.pcap
